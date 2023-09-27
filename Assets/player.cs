dbc5cc50999ba16134025c1b0b9fb3ac5ec4d508
Zaeem <zaeem.nadeem01@gmail.com>
2023-09-27 01:22:26 -0400

    New angle logic

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,23 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using Mono.CompilerServices.SymbolWriter;
+using Unity.VisualScripting;
 
 public class player : MonoBehaviour
 {
     Vector2 offset;
+    public float minAngle = -30f;    // Minimum angle in degrees
+    public float maxAngle = 30f;     // Maximum angle in degrees
+    public float rotationSpeed = 45f; // Rotation speed in degrees per second
 
-    // public bool Stop;
-    // private void Update()
-    // {
-    //     if (!Stop)
-    //         transform.position += (Vector3)Vector2.right * 3 * Time.deltaTime;
-    // }
-    // private void OnCollisionEnter2D(Collision2D other)
-    // {
-    //     Stop = true;
-    // }
+    public float currentAngle = 0f;
+    private int direction = 1;
+    public bool Stop;
+    public float newAngle;
+    private void Update()
+    {
+        if (!Stop)
+            transform.position += transform.right * 3 * Time.deltaTime;
+
+
+        else
+        {
+
+            newAngle = currentAngle + (rotationSpeed * direction * Time.deltaTime);
+            // If the new angle goes beyond the min or max, reverse direction
+            if (newAngle < minAngle || newAngle > maxAngle)
+            {
+                direction *= -1;
+            }
+            newAngle = Mathf.Clamp(newAngle, minAngle, maxAngle);
+            transform.rotation = Quaternion.Euler(0f, 0f, newAngle);
+            currentAngle = newAngle;
+            // Debug.Log(currentAngle);
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // transform.rotation = Quaternion.Euler(0f, 0f, currentAngle + 180);
+        newAngle = currentAngle + 180;
+        // 
+        minAngle = newAngle - 45;
+        maxAngle = newAngle + 45;
+        // Debug.Break();
+        Stop = true;
+    }
+
 
     [ContextMenu("test")]
     public void move()
